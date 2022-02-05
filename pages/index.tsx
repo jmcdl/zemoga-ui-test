de7d9fa5c1aaf68bd4e5ea8e99f79da27d9aaf6e
James McDonell <james.mcdonell@outlook.com>
2022-02-05 11:41:28 -0500

    Add index page

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+// import { Navbar } from "../components/Navbar";
+// import { Header } from "../components/Header";
+
+function HomePage() {
+  return (
+    <>
+      <Head>
+        <title>Rule of Thumb</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div>hello</div>
+      {/*<Navbar />*/}
+      {/*<Header />*/}
+    </>
+  );
+}
+
+export default HomePage;
