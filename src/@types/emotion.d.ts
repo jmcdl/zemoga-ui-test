1013fb1e543c865376be944f78eb70621c523f25
James McDonell <james.mcdonell@outlook.com>
2022-02-05 17:52:23 -0500

    update theme colors interface for better code completion

--- src/@types/emotion.d.ts
+++ src/@types/emotion.d.ts
@@ -3,8 +3,20 @@ import "@emotion/react";
 declare module "@emotion/react" {
   export interface Theme {
     breakpoints: { [key: string]: string };
-    colors: { [key: string]: string };
-    // fonts: { [key: string]: string };
+    colors: {
+      greenPositiveLight: string;
+      greenPositiveDark: string;
+      yellowPositiveLight: string;
+      yellowPositiveDark: string;
+      darkBackground: string;
+      darkerBackground: string;
+      darkerGray: string;
+      darkGray: string;
+      lightGray: string;
+      lightBackground: string;
+      lighterBackground: string;
+      white: string;
+    };
     constants: { [key: string]: string };
   }
 }
