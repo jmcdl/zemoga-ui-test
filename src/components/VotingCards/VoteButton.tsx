983208206cd0c2cb17e64ccb02f64599e781b489
James McDonell <james.mcdonell@outlook.com>
2022-02-07 11:13:28 -0500

    add error animation to vote button when no selection made

--- src/components/VotingCards/VoteButton.tsx
+++ src/components/VotingCards/VoteButton.tsx
@@ -1,6 +1,6 @@
 import { IconButton } from "../shared/styled-components";
-import { css, Theme } from "@emotion/react";
-import { Dispatch, SetStateAction } from "react";
+import { css, keyframes, Theme } from "@emotion/react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { SelectedVote } from "src/shared/interfaces";
 
 const voteButton = (
@@ -10,17 +10,27 @@ const voteButton = (
   height: 38px;
   min-width: 107px;
   color: ${theme.colors.white};
-  ${
-    selectedVote
-      ? `background-color: rgba(48, 48, 48, 0.6)`
-      : `background-color: ${theme.colors.darkerBackground}`
-  };
+  ${selectedVote
+    ? `background-color: rgba(48, 48, 48, 0.6)`
+    : `background-color: ${theme.colors.darkerBackground}`};
   border: 1px solid ${theme.colors.white};
   display: flex;
   align-items: center;
   justify-content: center;
   font-size: 14px;
   line-height: 18px;
+`;
+
+const shake = keyframes`  
+  25% {transform: rotate(3deg);}
+  50% {transform: rotate(-2deg);}
+  75% {transform: rotate(1deg);}
+  100% {transform: rotate(0deg);}
+`;
+
+const shakeAnimation = css`
+  animation-duration: 0.3s;
+  animation-name: ${shake};
 `;
 
 interface Props {
@@ -37,22 +47,31 @@ export function VoteButton({
   setHasVoted,
   submitVote,
 }: Props) {
+  const [showErrorShake, setShowErrorShake] = useState(false);
 
   const handleVoteClick = async () => {
     if (hasVoted) {
       setHasVoted(false);
-    } else {
-      setHasVoted(true);
-      setVoteSelection(null)
-      await submitVote(selectedVote);
+      return;
     }
+    if (!selectedVote) {
+      setShowErrorShake(true);
+      return;
+    }
+    setHasVoted(true);
+    setVoteSelection(null);
+    await submitVote(selectedVote);
   };
 
   return (
     <IconButton
       aria-label={hasVoted ? "Vote Again" : "Vote Now"}
-      css={(theme) => voteButton(theme, { selectedVote })}
+      css={[
+        (theme) => voteButton(theme, { selectedVote }),
+        showErrorShake && shakeAnimation,
+      ]}
       onClick={handleVoteClick}
+      onAnimationEnd={() => setShowErrorShake(false)}
     >
       {hasVoted ? "Vote Again" : "Vote Now"}
     </IconButton>
